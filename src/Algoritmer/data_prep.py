89cc72bd0a21566a9df462ab0adfa63d90b8f85b
Kat <s183910@student.dtu.dk>
2021-09-24 16:23:50 +0200

    ranknet data prep igang

--- src/Algoritmer/data_prep.py
+++ src/Algoritmer/data_prep.py
@@ -1,23 +1,40 @@
-import tensorflow as tf
-from tensorflow.keras import layers, activations, losses, Model, Input
-from tensorflow.nn import leaky_relu
-import numpy as np
+
 from itertools import combinations
-from tensorflow.keras.utils import plot_model, Progbar
-import matplotlib.pyplot as plt
+# from tensorflow.keras.utils import plot_model, Progbar
+# import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+# import tensorflow as tf
+# from tensorflow.keras import layers, activations, losses, Model, Input
+from tensorflow.nn import leaky_relu
 import pdb
 # import mymodule
 
-# generate data
-nb_query = 20
-query = np.array([i+1 for i in range(nb_query) for x in range(int(np.ceil(np.abs(np.random.normal(0,scale=15))+2)))])
-doc_features = np.random.random((len(query), 10))
-# Vi mener, at doc_features=documenter=grades, gender, stratum etc.
+# # generate data
+# nb_query = 20
+# query = np.array([i+1 for i in range(nb_query) for x in range(int(np.ceil(np.abs(np.random.normal(0,scale=15))+2)))])
+# doc_features = np.random.random((len(query), 10))
+# # Vi mener, at doc_features=documenter=grades, gender, stratum etc.
 
-doc_scores = np.random.randint(5, size=len(query)).astype(np.float32)
-# Vi mener, at scores netop er de qvantitative værdier for hvert document for hver student.
+# doc_scores = np.random.randint(5, size=len(query)).astype(np.float32)
+# # Vi mener, at scores netop er de qvantitative værdier for hvert document for hver student.
 
+
+# Loading the data
+df = pd.read_csv("src/Algoritmer/df_sum_score_py.csv")
+
+# Creating the data
+# features
+df_document_features = df.copy()
+df_document_features = df_document_features[["GENDER_bin", "HI_GRADE_AVG"]]
+df_document_features = df_document_features.values.tolist()
+doc_features = np.array(df_document_features)
+#scores
+doc_scores = df["COL_GRADE_AVG"].values
+
+query = df.index.values
 
 
 
@@ -27,11 +44,11 @@ xj = []
 pij = []
 pair_id = []
 pair_query_id = []
+pdb.set_trace()
 for q in np.unique(query):
-    query_idx = np.where(query == q)[0]
-    # pdb.set_trace()
+    query_idx = np.where(query == q)[0] # den når hertil og har empty query_idx
+    pdb.set_trace()
     for pair_idx in combinations(query_idx, 2):
-        # pdb.set_trace()
         pair_query_id.append(q)
         pair_id.append(pair_idx)
         i = pair_idx[0]
@@ -47,7 +64,7 @@ for q in np.unique(query):
             _pij = 0
         pij.append(_pij)
 
-pdb.set_trace()
+# pdb.set_trace()
 
 xi = np.array(xi)
 xj = np.array(xj)
